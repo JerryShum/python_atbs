da4d5477c9132eafa201351bb83ac30338c2bf2f
JerryWSL <jerryshum2636@gmail.com>
2026-05-03 15:49:23 -0400

    chapter 1 -> basics of python

--- 1_basics/1_basic.py
+++ 1_basics/1_basic.py
@@ -1,1 +1,20 @@
-2+2
+# This program says hello and asks for my name.
+
+# print('Hello, World!')
+# print('What is your name?')
+# my_name = input('>')
+# print('It is good to meet you, ' + my_name)
+# print('The length of your name is:')
+# print(len(my_name))
+# print('What is your age?')  # Ask for their age.
+# my_age = input('>')
+# print('You will be ' + str(int(my_age) + 1) + ' in a year.')
+
+
+# Experimenting with types
+type(42) # int
+type(42.0) # float
+type('forty two') # string
+name = 'zophie'
+type(name) # name --> string type
+type(len(name)) #returns the type of the returned value of the function --> int
