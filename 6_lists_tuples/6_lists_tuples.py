5f764eef5cb82996fd78ae99c07a0c3ecab1a7d1
JerryShum <jerryshum2636@gmail.com>
2026-05-05 18:40:14 -0400

    chapter 6 - lists done

---
+++ 6_lists_tuples/6_lists_tuples.py
@@ -0,0 +1,24 @@
+# Practice:
+# Slices:
+
+spam = [1,2,3,4]
+
+# start at index 1, go upto 4 (not including)
+print(spam[1:4])
+
+# start at index 1 --> go to the end
+print(spam[1:])
+
+# start at 0 (start) --> go until not including index 3
+print(spam[:3])
+
+# start at 0 --> go until the LAST index (not including)
+print(spam[0:-1])
+
+# start at 0 --> go until not including index 2 [0,1]
+print(spam[:2])
+
+# ----
+
+# Concatenation & Replication
+# you can concatenate and replicate lists
